d7fe2a71bd984ab39c1dfa0453ab4bc72fe3238e
pearpages <pere@soms.cat>
2016-06-23 12:30:45 +0200

    typings

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
-/// <reference path="./src/utilities/utilityFunctions.ts" />
 /// <reference path="lodash.d.ts" />
 
 // this can only work on a client not in node, because is trying to use more than one module per file
-import util = Utility.Fees;
+import {calculateLateFee} from './lib/utilityFunctions';
 import * as _ from 'lodash';
 
 let snakeCaseTitle = _.snakeCase('For Whom the Bell Tolls');
 console.log(snakeCaseTitle);
 
-let fee = util.calculateLateFee(2);
+let fee = calculateLateFee(2);
 console.log(`Fee: ${fee}`)
